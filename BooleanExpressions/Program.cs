ec508dc54347bfb3cc21ebb0ba1194ddd1e4c389
William-M3 <WilliamTruMurphy@gmail.com>
2025-04-16 10:32:17 -0400

    More C# challenges probelms

---
+++ BooleanExpressions/Program.cs
@@ -0,0 +1,32 @@
+﻿// Random coin = new Random();
+// Console.WriteLine(coin.Next(0,2) == 1? "heads": "tails");
+
+string permission = "Admin|Manager";
+int level = 55;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level > 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+   Console.WriteLine("You do not have sufficient privileges."); 
+}
